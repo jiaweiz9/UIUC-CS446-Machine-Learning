780b688b850be42f3772b65869087492224f0ee0
Jiawei <1442730983@qq.com>
2024-03-10 16:03:25 -0500

    hw3 initial commit

---
+++ hw3-release/code/resnet.py
@@ -0,0 +1,47 @@
+import torch.nn as nn
+
+
+class Block(nn.Module):
+    """A basic block used to build ResNet."""
+
+    def __init__(self, num_channels):
+        """Initialize a building block for ResNet.
+
+        Argument:
+            num_channels: the number of channels of the input to Block, and is also
+                          the number of channels of conv layers of Block.
+        """
+        super(Block, self).__init__()
+
+    def forward(self, x):
+        """
+        The input will have shape (N, num_channels, H, W),
+        where N is the batch size, and H and W give the shape of each channel.
+
+        The output should have the same shape as input.
+        """
+        pass
+
+
+class ResNet(nn.Module):
+    """A simplified ResNet."""
+
+    def __init__(self, num_channels, num_classes=10):
+        """Initialize a shallow ResNet.
+
+        Arguments:
+            num_channels: the number of output channels of the conv layer
+                          before the building block, and also 
+                          the number of channels of the building block.
+            num_classes: the number of output units.
+        """
+        super(ResNet, self).__init__()
+
+    def forward(self, x):
+        """
+        The input will have shape (N, 1, H, W),
+        where N is the batch size, and H and W give the shape of each channel.
+
+        The output should have shape (N, 10).
+        """
+        pass
